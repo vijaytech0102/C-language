ee397469fbacc34706cc3f133967551cecace8a5
Vijay Sen Singh Yadav <91178268+vijaytech0102@users.noreply.github.com>
2025-07-12 11:57:56 +0530

    Create fibonaci.c

---
+++ fibonaci.c
@@ -0,0 +1,23 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void fib(int range)
+{
+    long int a=0;
+    long int b=1;
+    printf("1. %d \n2. %d\n",a,b);
+    for( int i=3; i<=range; i++)
+    {
+        long int c=a+b;
+        a=b;
+        b=c;
+        printf(" %d.%d\n",i, c);
+    }
+}
+int main() {
+    // Write C code here
+    int range;
+    printf("Enter the range for the series:");
+   scanf("%d", &range);
+   fib(range);
+    return 0;
+}
